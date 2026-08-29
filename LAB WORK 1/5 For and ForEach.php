4534ea3743b96c831ca19906cd9acd2fb8530784
sharmandevesh1-ship-it <sharmandevesh1@gmail.com>
2026-08-29 21:58:03 +0530

    Add files via upload

---
+++ LAB WORK 1/5 For and ForEach.php
@@ -0,0 +1,15 @@
+<?php
+
+    echo "5 to 10 using For Loop <br><br>";
+    for($i=5;$i<=10;$i++)
+        echo $i."<br>";
+    
+    echo "<br> 5 to 10 using ForEach Loop <br>";
+
+    $numbers = range(5, 10); // range() generates an array from 5 to 10
+
+    foreach ($numbers as $num) {
+    echo $num . "<br>";
+}
+
+?>
